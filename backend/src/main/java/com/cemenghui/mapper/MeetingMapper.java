c9366e4ed72ac575c1d1aa8c034abd5f5ce71a92
20236829 <375054441@qq.com>
2025-06-28 13:14:18 +0800

    add MeetingMapper
    添加了会议实体类的数据库接口

---
+++ backend/src/main/java/com/cemenghui/mapper/MeetingMapper.java
@@ -0,0 +1,9 @@
+package com.cemenghui.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.cemenghui.entity.Meeting;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface MeetingMapper extends BaseMapper<Meeting> {
+} 
