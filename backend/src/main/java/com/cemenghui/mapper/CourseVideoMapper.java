e720359615350d6dda3b81ac8c2abd21b0288f93
20236829 <375054441@qq.com>
2025-06-28 13:28:12 +0800

    删除没用到的import
    将没用到的操作数据库的接口删除，以防后续加载错误或错误版本的接口导致程序出错

--- backend/src/main/java/com/cemenghui/mapper/CourseVideoMapper.java
+++ backend/src/main/java/com/cemenghui/mapper/CourseVideoMapper.java
@@ -3,10 +3,6 @@ package com.cemenghui.mapper;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.cemenghui.entity.CourseVideo;
 import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Param;
-import org.apache.ibatis.annotations.Select;
-
-import java.util.List;
 
 @Mapper
 public interface CourseVideoMapper extends BaseMapper<CourseVideo> {
--- backend/src/main/java/com/cemenghui/mapper/UserVideoProgressMapper.java
+++ backend/src/main/java/com/cemenghui/mapper/UserVideoProgressMapper.java
@@ -3,10 +3,7 @@ package com.cemenghui.mapper;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.cemenghui.entity.UserVideoProgress;
 import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Param;
-import org.apache.ibatis.annotations.Select;
 
-import java.util.List;
 
 @Mapper
 public interface UserVideoProgressMapper extends BaseMapper<UserVideoProgress> {
